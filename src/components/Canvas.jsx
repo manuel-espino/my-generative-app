3aa30feb588b73c2efbc808abc201e97a946addf
ManuelEspino <manudrago@gmail.com>
2022-06-27 23:37:46 +0200

    Download pgn

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import saveSvgAsPng from "save-svg-as-png";
+import {saveSvgAsPng} from "save-svg-as-png";
 
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
@@ -17,8 +17,9 @@ width: 100vw;
 height: 100vh;
 display: flex;
 flex-wrap: wrap;
-align-items: flex-start;
+align-items: center;
 align-content: flex-start;
+justify-content: center;
 `;
 
 const Celda = styled.div`
@@ -27,21 +28,32 @@ height: 10vw;
 background-color: rgba(${random_color}, ${random_color}, ${random_color}, 1);
 `;
 
+const BotonDescargar = styled.button`
+background-color: blue;
+padding:20px 30px;
+border-radius: 1000px;
+border: none;
+color: white;
+font-size: 20px;
+font-weight: bold;
+cursor: pointer;
+margin: 10px;
 
+`;
    /*<rect fill={ `rgba(${random_color()}, ${random_color()}, ${random_color()}, 1)`} width="10" height="10" x={ `${i*10}`} y="0" stylee={ `${i}`} />*/
  
 const ObjetosSVG = () => {
 const objetos = [...Array(10)].map((_, i) => [...Array(10)]);
 
  return objetos.map((fila, i) => 
- fila.map((_, j) =>     <rect  key={random_color()*i} 
+ fila.map((_, j) =>     <rect  key={random_color()*random_color()} 
     stylee={"col"+ j + "fila" +i} fill={ `rgba(${random_color()}, ${random_color()}, ${random_color()}, 1)`} 
-    width="10" height="10" x={j*10} y={i*10} />
+    width="100" height="100" x={j*100} y={i*100} />
     ));
 }
 
 const guardarPNG = () => {
-    saveSvgAsPng(document.getElementById("canvas"), "canvas.png");
+    saveSvgAsPng(document.getElementById("elemento-svg"), "canvas.png");
 }
 
 function Canvas() {
@@ -49,15 +61,15 @@ function Canvas() {
         <Contenedor className="pp">
         <div className="flexbox-container">
             <div>
-                <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                <svg id="elemento-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
                     <g>
                         <ObjetosSVG/>
                     </g>
                 </svg>
             </div>
         </div>
-                <button onClick={guardarPNG()}>Download Image</button>
 
+<BotonDescargar onClick={guardarPNG}>Download</BotonDescargar>
         </Contenedor>
     );
 }
