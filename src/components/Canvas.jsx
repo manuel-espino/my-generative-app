07068a0bb219b2d62dcf9e524c63a2b765725550
ManuelEspino <manudrago@gmail.com>
2022-06-27 16:35:27 +0200

    First attempt to make a generative color canvas and create an svg with this image.

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,25 +1,49 @@
 import React from "react";
+import { elementToSVG } from 'dom-to-svg'
 import styled from "styled-components";
 
 
 
-const random_color = () => {
-    return 'blue';
+// Capture the whole document
+const svgDocument = elementToSVG(document.querySelector('#root'))
+const svgString = new XMLSerializer().serializeToString(svgDocument)
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
+const random_color = () => {
+    return randomNumber(0, 255);
+}
+
+const n = 100; // Or something else
+
+const Contenedor = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    align-content: flex-start;
+`;
 
 const Celda = styled.div`
-    width: 100px;
-    height: 100px;  
-    background-color: lhs(${random_color}
+    width: 10vw;
+    height: 10vw;  
+    background-color: rgba(${random_color}, ${random_color}, ${random_color}, 1);
 `;
+
 
 function Canvas() {
     return (
-        <div className="canvas">
-            <Celda />
-        </div>
+        <Contenedor className="pp">
+            {[...Array(n)].map((e, i) => <Celda />
+            )
+            }
+            {svgString}
+        </Contenedor>
     );
 }
+
 
 export default Canvas;
